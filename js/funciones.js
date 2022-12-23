9443124a3a47d5ea53913eaf706efb36beea424d
pmarmor <politotuto@gmail.com>
2022-12-23 18:28:12 +0100

    botón nieve eliminado, decksp keywords

--- js/funciones.js
+++ js/funciones.js
@@ -11,6 +11,7 @@ function cambiaImagenSlider(boton) {
     let imagenes = ['url(img/sp2.png)','url(img/DeckLogoGold.png)','url(img/DeckLogoMagenta.png)'];
     let colores = ["rgba(0, 102, 255, 0.932)","rgba(255, 0, 0, 0.8)","rgba(255, 102, 0, 0.932)"];
     let titulos=["Bienvenidos a Deck Sp","Mejores ofertas para esta navidad","¿Qué hacer si tu Steam Deck no enciende?"];
+    let enlaces=["noticias/bienvenida.html","",""];
     let i = 0;
     imagenes.forEach(elemento => {
         let rutaRelativa=elemento
@@ -26,6 +27,7 @@ function cambiaImagenSlider(boton) {
         }
         document.getElementById(sliderId).style.backgroundImage = imagenes[i + 1]
         document.getElementById("sliderLink").style.backgroundColor = colores[i + 1]
+        document.getElementById("sliderLink").href = enlaces[i + 1]
         document.getElementById("content").innerHTML=titulos[i+1]
     }
     else {
@@ -34,6 +36,7 @@ function cambiaImagenSlider(boton) {
         }
         document.getElementById(sliderId).style.backgroundImage = imagenes[i - 1]
         document.getElementById("sliderLink").style.backgroundColor = colores[i - 1]
+        document.getElementById("sliderLink").href = enlaces[i + 1]
         document.getElementById("content").innerHTML=titulos[i-1]
     }
     if(typeof boton.id !="undefined"){
