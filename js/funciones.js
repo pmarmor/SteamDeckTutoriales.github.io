8fd371d751060e182b094002b8fef87bec15ff19
pmarmor <politotuto@gmail.com>
2022-12-25 21:02:37 +0100

    nuevo sitemap

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,6 @@
 "use strict"
+document.getElementById("content").innerHTML="Bienvenidos a DeckSP"
+document.getElementById("sliderLink").href="noticias/bienvenida.html"
 let crono = setInterval(cronometro, 4000)//Cronometro del slider. Necesario para que se ejecute por primera vez, ya que luego se reinicia en
 //la función cambiaImagenSlider
 
@@ -7,10 +9,10 @@ function cambiaImagenSlider(boton) {
     crono = setInterval(cronometro, 4000)//Volvemos con el valor inicial del cronometro después de borrar el crono de 8 segundos
     let sliderId = getSliderId()
     let img = getImgPath()
-    let imagenes = ['url(img/sp2.png)', 'url(img/DeckLogoGold.png)', 'url(img/DeckLogoMagenta.png)'];
-    let colores = ["rgba(0, 102, 255, 0.932)", "rgba(255, 0, 0, 0.8)", "rgba(255, 102, 0, 0.932)"];
-    let titulos = ["Bienvenidos a Deck Sp", "Mejores ofertas para esta navidad", "¿Qué hacer si tu Steam Deck no enciende?"];
-    let enlaces = ["noticias/bienvenida.html", "", ""];
+    let imagenes = ['url(img/DeckLogoGold.png)', 'url(img/DeckLogoMagenta.png)','url(img/sp2.png)'];
+    let colores = ["rgba(255, 0, 0, 0.8)", "rgba(255, 102, 0, 0.932)","rgba(0, 102, 255, 0.932)" ];
+    let titulos = [ "Mejores ofertas para esta navidad", "¿Qué hacer si tu Steam Deck no enciende?","Bienvenidos a Deck Sp"];
+    let enlaces = [ "", "","noticias/bienvenida.html"];
     let i = 0;
     imagenes.forEach(elemento => {
         let rutaRelativa = elemento
@@ -29,6 +31,10 @@ function cambiaImagenSlider(boton) {
         }
         else { document.getElementById(sliderId).style.backgroundImage = imagenes[i + 1] }
         document.getElementById("sliderLink").style.backgroundColor = colores[i + 1]
+        console.log("--------")
+        console.log(enlaces.length)
+        console.log(i+1)
+        console.log(enlaces[i+1])
         document.getElementById("sliderLink").href = enlaces[i + 1]
         document.getElementById("content").innerHTML = titulos[i + 1]
     }
@@ -37,8 +43,12 @@ function cambiaImagenSlider(boton) {
             i = imagenes.length
         }
         document.getElementById(sliderId).style.backgroundImage = imagenes[i - 1]
+        console.log("--------")
+        console.log(enlaces.length)
+        console.log(i-1)
+        console.log(enlaces[i-1])
         document.getElementById("sliderLink").style.backgroundColor = colores[i - 1]
-        document.getElementById("sliderLink").href = enlaces[i + 1]
+        document.getElementById("sliderLink").href = enlaces[i - 1]
         document.getElementById("content").innerHTML = titulos[i - 1]
     }
     if (typeof boton.id != "undefined") {
