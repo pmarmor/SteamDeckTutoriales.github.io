e4b11e03e4564b0d098d7d994bf1673f2ec379b3
pmarmor <100232112+pmarmor@users.noreply.github.com>
2023-04-04 19:01:37 +0200

    adaptación a 4k

--- js/navegacion.js
+++ js/navegacion.js
@@ -1,7 +1,12 @@
 let posicionAnterior = 0
+window.onscroll = scrollFunction;
 function scrollFunction() {
     let posicionActual = document.documentElement.scrollTop
-    let headerId = getHeaderId();
+    let headerId
+    if (typeof headerId == 'undefined') {
+        headerId = getHeaderId();    
+}
+if(screen.width<2000){
     if (posicionActual > 40) {  //screen.width < 800 &&
         if (posicionAnterior < posicionActual) { //baja
             document.getElementById(headerId).style.transition = 'all 0.6s';
@@ -9,7 +14,7 @@ function scrollFunction() {
             posicionAnterior = posicionActual
 
         }
-        if(posicionAnterior > posicionActual+60) {   //sube     //no aparece hasta que suba 60px
+        if(posicionAnterior > posicionActual+50) {   //sube     //no aparece hasta que suba 50px
             posicionAnterior = posicionActual
             document.getElementById(headerId).style.transition = 'all 0.4s';
             document.getElementById(headerId).style.marginTop = '0px';
@@ -19,8 +24,8 @@ function scrollFunction() {
         document.getElementById(headerId).style.marginTop = '0px';
     }
 }
+}
 function getHeaderId() {
     let header = document.getElementsByTagName("header")
     return header[0].id
 }
-window.onscroll = scrollFunction;
